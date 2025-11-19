75da1197a69d8434aa3f0f79308ff41adb4f7e40
officialsurajraj <surajraj41187@gmail.com>
2025-11-19 15:25:07 +0530

    user controllers completed now

--- backend/src/controllers/user.controllers.js
+++ backend/src/controllers/user.controllers.js
@@ -1,4 +1,5 @@
 import { User } from "../models/user.models.js"
+import { uploadOnCloudinary } from "../utils/cloudinary.js"
 
 
 const generateAccessToken = async (userId) => {
@@ -81,7 +82,7 @@ export const logout = async (req, res) => {
 export const uploadImage = async (req, res) => {
     try {
         const image = req.file?.path
-        console.log(image)
+
         if (!image) {
             return res.status(400).json({ message: "please upload image" })
         }
@@ -101,4 +102,53 @@ export const uploadImage = async (req, res) => {
         console.log(`Image Uploading Failed now : ${error}`)
     }
 
+}
+
+export const changeCurrentPassword = async (req, res) => {
+    const { oldPassword, newPassword } = req.body;
+
+    if (!oldPassword || !newPassword) {
+        return res.status(400).json({ message: "Old and new password are required" })
+    }
+    const user = await User.findById(req.user._id);
+
+    if (!user) {
+        return res.status(404).json({ message: "User not found" });
+    }
+    const isPasswordCorrect = await user.isPasswordCorrect(oldPassword)
+    if (!isPasswordCorrect) {
+        return res.status(401).json({ message: "Incorrect current password" });
+    }
+    user.password = newPassword;
+    await user.save()
+
+    return res.status(200).json({ message: "password Successfully Chanaged" })
+
+}
+export const getCurrentUser = async (req, res) => {
+    return res.status(200).json(
+        {
+            message: "Current User data",
+            data: req.user
+        }
+    )
+}
+export const updateAccountDetails = async (req, res) => {
+    const { name, description } = req.body
+    if (!name || !description) {
+        return res.status(400).json({ message: "Please Enter your information" })
+    }
+    const user = await User.findByIdAndUpdate(req.user._id,
+        {
+            $set: {
+                name: name,
+                description: description
+            }
+        },
+        {
+            new: true
+        }
+    ).select("-password")
+
+    return res.status(200).json({ message: "User information update successflly now", user })
 }
--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -3,11 +3,16 @@ const router = express.Router();
 import { verifyJWT } from "../middleware/auth.middleware.js"
 import { upload } from "../middleware/multer.middleware.js"
 
-import { register, login, logout, uploadImage } from "../controllers/user.controllers.js"
+import { register, login, logout, uploadImage, changeCurrentPassword, getCurrentUser, updateAccountDetails } from "../controllers/user.controllers.js"
 
 router.route("/register").post(register);
-router.route("/login").post(login)
-router.route("/logout").post(verifyJWT, logout)
-router.route("/upload-profile").patch(verifyJWT, upload.single("photoUrl"), uploadImage)
+router.route("/login").post(login);
+router.route("/logout").post(verifyJWT, logout);
+router.route("/upload-profile").patch(verifyJWT, upload.single("photoUrl"), uploadImage);
+router.route("/change-password").patch(verifyJWT, changeCurrentPassword);
+router.route("/profile").get(verifyJWT, getCurrentUser);
+router.route("/update-info").patch(verifyJWT, updateAccountDetails);
+
+
 
 export default router
